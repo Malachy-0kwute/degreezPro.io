023c6151bf643dcc6473371fbc859b01e4a5790a
Malachy Okwute <mokwute@ivytech.edu>
2026-04-23 12:29:49 -0400

    added item template to cities page

--- js/services/navService.js
+++ js/services/navService.js
@@ -48,7 +48,8 @@ export default class NavigationService {
     document.getElementById('homepage-menu-btn').click();
 
     // Set default page to home
-    this.navigateToPage(home, false);
+    // this.navigateToPage(home, false);
+    this.navigateToPage(cities, false);
   }
   
 }
