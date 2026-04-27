b596d44b15dce6989db113b988e21debd7df4cbb
Malachy Okwute <mokwute@ivytech.edu>
2026-04-27 09:50:42 -0400

    added notification template and structure

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ import * as homeContent from "homePage";
 const navService = new NavigationService();
 navService.initialize();
 
+function toggleAddLocationPopup() {
+  const sideMenu = document.querySelector('.add-location-popup');
+  sideMenu.classList.toggle('active');
+}
+
+function toggleNotificationPopup() {
+  const notificationPopup = document.querySelector('.notification');
+  notificationPopup.classList.toggle('active');
+}
+
 // toggle side menu when collapsed
 document.querySelector('.menu-button').addEventListener('click', () => {
   const sideMenu = document.querySelector('aside');
@@ -15,7 +25,7 @@ document.querySelector('.menu-button').addEventListener('click', () => {
 document.querySelector('.add-location-button').addEventListener('click', toggleAddLocationPopup);
 document.querySelector('.submit').addEventListener('click', toggleAddLocationPopup);
 
-function toggleAddLocationPopup() {
-  const sideMenu = document.querySelector('.add-location-popup');
-  sideMenu.classList.toggle('active');
-}
+// toggle notification popup
+document.querySelector('.notification-button').addEventListener('click', toggleNotificationPopup);
+document.querySelector('.notification-button').addEventListener('blur', toggleNotificationPopup); // hide notification popup when it loses focus
+
--- js/pages/home.js
+++ js/pages/home.js
@@ -1,6 +1,7 @@
 import { home } from "navlinks";
 import getLocationCoordinates from "apiService";
 import * as helpers from "utilities";
+import { radar } from "navlinks";
 
 // set current location name
 export async function setCurrentLocationName(location = "Fort Wayne") {
@@ -8,10 +9,8 @@ export async function setCurrentLocationName(location = "Fort Wayne") {
   locationElement.textContent = location;
 }
 
-async function getLocation()
-{
+async function getLocation() {
   const location = await getLocationCoordinates("Fort Wayne");
-  console.log(location);
 }
 
 getLocation();
